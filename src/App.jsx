654b8803b19fb36f531d5727c2379eb108becf77
Eliz Erdim <elizerdim@gmail.com>
2024-05-08 02:14:45 +0300

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
+
 export default function App() {
   return (
     <>
-      
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
   )
 }
