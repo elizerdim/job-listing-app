08955f706c93d3b3295e80c1b72994725947217c
Eliz Erdim <elizerdim@gmail.com>
2024-05-08 05:09:00 +0300

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import { 
+  Route, 
+  createBrowserRouter, 
+  createRoutesFromElements,
+  RouterProvider 
+} from 'react-router-dom'
+import MainLayout from "./layouts/MainLayout"
+import HomePage from "./pages/HomePage"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+)
 
 export default function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
