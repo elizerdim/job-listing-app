7bba0eb717606f6c5fcb8f26d81ad35f43e58c99
Eliz Erdim <elizerdim@gmail.com>
2024-05-08 20:25:52 +0300

    Add jobs page content

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,5 +1,9 @@
+import JobListings from "../components/JobListings"
+
 export default function JobsPage() {
   return (
-    <div>Jobs Page</div>
+    <section className="bg-blue-50 px-4 py-6">
+      <JobListings />
+    </section>
   )
 }
