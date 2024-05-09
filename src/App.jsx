eed352662b34fdcaa25fe243b85e03b2d565a95e
Eliz Erdim <elizerdim@gmail.com>
2024-05-09 15:30:41 +0300

    Move router inside App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { 
-  Route, 
-  createBrowserRouter, 
+import {
+  Route,
+  createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider 
-} from 'react-router-dom'
-import MainLayout from "./layouts/MainLayout"
-import HomePage from "./pages/HomePage"
-import JobsPage from "./pages/JobsPage"
-import JobPage from './pages/JobPage'
-import jobLoader from './utils/jobLoader'
-import AddJobPage from './pages/AddJobPage'
-import NotFoundPage from './pages/NotFoundPage'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-      <Route path="/add-job" element={<AddJobPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-)
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import JobPage from "./pages/JobPage";
+import jobLoader from "./utils/jobLoader";
+import AddJobPage from "./pages/AddJobPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
-  return <RouterProvider router={router} />
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="/add-job" element={<AddJobPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
